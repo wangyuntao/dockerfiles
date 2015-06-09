7cbb92e5d67201555c8f8b7ff8bfb8be469a215a
wyt <wyt.daily@gmail.com>
2015-06-10 00:23:54 +0800

    1

---
+++ percona/docker/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+if [ "$1" = 'mysqld' ]; then
+	DATADIR="$("$@" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
+
+	if [ ! -d "$DATADIR/mysql" ]; then
+		mysql_install_db --datadir="$DATADIR"
+			
+		mysqlInitFile='/docker/mysql-init-file.sql'
+		cat > "$mysqlInitFile" <<-EOF
+			GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+		EOF
+		
+		if [ "$DB" ]; then
+			echo "CREATE DATABASE IF NOT EXISTS \`$DB\` ;" >> "$mysqlInitFile"
+		fi
+
+		set -- "$@" --init-file="$mysqlInitFile"
+	fi
+
+	chown -R mysql:mysql "$DATADIR"
+fi
+
+exec "$@"
