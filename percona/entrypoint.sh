ffa8749d6a8e57f09e7ade8a5468ae50ebfc2ae0
wyt <wyt.daily@gmail.com>
2015-06-10 02:31:48 +0800

    1

--- percona/entrypoint.sh
+++ percona/entrypoint.sh
@@ -14,7 +14,7 @@ if [ "$1" = 'mysqld' ]; then
 			EOF
 			
 			if [ "$MYSQL_DATABASE" ]; then
-				echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" >> "$mysqlInitFile"
+				echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;" >> "$mysqlInitFile"
 			fi
 		fi
 		set -- "$@" --init-file="$mysqlInitFile"
